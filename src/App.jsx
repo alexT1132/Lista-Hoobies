ae2dd179f7e30ee4af64ff31f9fc2b2f80a2d639
Alex <112890691+alexT1132@users.noreply.github.com>
2023-06-06 17:54:22 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import './App.css'
+
+function App() {
+  const nombre = "Alejandro Tellez Vazquez"
+
+  return (<div>
+    <h3>Lista de Hobbies de {nombre} </h3>
+ 
+    <ol>
+      <li> Salir con amigos </li>
+      <li> Jugar futbol </li>
+      <li> Ver peliculas </li>
+      <li> Dormir </li>
+      <li> Comer </li>
+    </ol>
+    </div>)
+  
+}
+
+export default App
